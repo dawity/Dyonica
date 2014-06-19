3165ab4f9cca1c74b69804b7722a57f32b31793d
Dawity <dawityehaile@gmail.com>
2014-06-18 23:19:29 -0700

    Added user and set SSL=true

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  resources :users
+  
+
+
+  match '/signup',  to: 'users#new', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
